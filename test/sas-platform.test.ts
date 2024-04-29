31cc52ee67862cf88310ca47194ea5981d8fb307
Vladimir Pasquier <1112241+vpasquier@users.noreply.github.com>
2024-04-29 16:59:15 -0400

    Initial commit

---
+++ test/sas-platform.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SasPlatform from '../lib/sas-platform-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sas-platform-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SasPlatform.SasPlatformStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
